a2ece4991a0f2c329d0964413062d13baa95c673
MuskanG26 <muskan.gupta_cs19@gla.ac.in>
2020-08-29 20:36:14 +0530

    creating non parameterized constructor Book CREATING PARAMETERIZED CONSTRUCTOR BOOK() adding method for checking ISBN_NO

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -11,18 +11,29 @@ public class Book {
     private String AuthorName;
     private String ISBN_NO;
 
-    public Book()
-    {
-        BookName="The Alchemist";
-        AuthorName="Paulo Coelho";
-        ISBN_NO="2ABA23728856N";
-    }
-    public Book(String BookName, String AuthorName, String ISBN_NO)
-    {
-        this.BookName=BookName;
-        this.AuthorName=AuthorName;
-        this.ISBN_NO=ISBN_NO;
+    public Book() {
+        BookName = "The Alchemist";
+        AuthorName = "Paulo Coelho";
+        ISBN_NO = "2ABA23728856N";
     }
 
+    public Book(String BookName, String AuthorName, String ISBN_NO) {
+        this.BookName = BookName;
+        this.AuthorName = AuthorName;
+        this.ISBN_NO = ISBN_NO;
+    }
 
+    /**
+     * method for checking length of ISBN NUMBER.
+     */
+
+
+    public void checkISBN(String ISBN_No) {
+        int N = ISBN_No.length();
+        if (N != 13) {
+            System.out.println("Invalid ISBN Number, enter again!");
+        }
+
+
+    }
 }
