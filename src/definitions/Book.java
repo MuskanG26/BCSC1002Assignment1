d22228ae2713fe04a2e37ce26b597603f1d30580
MuskanG26 <muskan.gupta_cs19@gla.ac.in>
2020-08-29 19:14:10 +0530

    creating non parameterized constructor Book()

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -7,4 +7,16 @@
 package definitions;
 
 public class Book {
+    private String BookName;
+    private String AuthorName;
+    private String ISBN_NO;
+
+    public book ()
+    {
+        BookName="The Alchemist";
+        AuthorName="Paulo Coelho";
+        ISBN_NO="2ABA23728856N";
+    }
+
+
 }
